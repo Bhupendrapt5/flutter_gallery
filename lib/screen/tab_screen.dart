22b310cf6530a3a8d1b18dedc876582a4cd1b092
bhupendrapt <bhupendrapt95@gmail.com>
2020-07-10 22:14:47 +0530

    Implemented Tab

--- lib/screen/tab_screen.dart
+++ lib/screen/tab_screen.dart
@@ -1,11 +1,57 @@
+import 'package:cam_gal/screen/cam_screen.dart';
+import 'package:cam_gal/screen/galley_screen.dart';
 import 'package:flutter/material.dart';
 
-class TabScreen extends StatelessWidget {
+class TabScreen extends StatefulWidget {
+  @override
+  _TabScreenState createState() => _TabScreenState();
+}
+
+class _TabScreenState extends State<TabScreen> {
+  final List<Map<String, Object>> _pages = [
+    {
+      'page': CameraScreen(),
+      'title': 'Take a Pic',
+    },
+    {
+      'page': GalleryScreen(),
+      'title': 'Your Images',
+    },
+  ];
+  int _selectedPageIndex = 0;
+
+  void _selectPage(int index) {
+    setState(() {
+      _selectedPageIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Hola'),
+      appBar: AppBar(
+        title: Text(_pages[_selectedPageIndex]['title']),
+        backgroundColor: Theme.of(context).primaryColor,
+      ),
+      body: _pages[_selectedPageIndex]['page'],
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _selectPage,
+        backgroundColor: Theme.of(context).primaryColor,
+        unselectedItemColor: Colors.black,
+        selectedItemColor: Colors.red,
+        currentIndex: _selectedPageIndex,
+        items: [
+          BottomNavigationBarItem(
+            backgroundColor: Theme.of(context).primaryColor,
+            icon: Icon(Icons.camera),
+            title: Text('Camera'),
+          ),
+          BottomNavigationBarItem(
+            backgroundColor: Theme.of(context).primaryColor,
+            icon: Icon(Icons.image),
+            title: Text('Images'),
+          ),
+        ],
       ),
     );
   }
